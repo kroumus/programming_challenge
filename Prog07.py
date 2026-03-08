d84deccbb2aad972ad7dfe2012f29b86df1b54da
Rae <aldrichpadilla404@gmail.com>
2026-03-09 03:04:26 +0800

    fixed input function and finished

--- Prog07.py
+++ Prog07.py
@@ -1,5 +1,10 @@
 #Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 #input 10 numbers
+total_sum = 0
 for i in range(10):
-    int(input("Enter a number: "))
+    number = int(input(f"Enter number {i+1}: "))
+    total_sum += number
+
+#Print the sum of all the numbers.
+print(total_sum)
