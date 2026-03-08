a462af0f94ec7482a5256708233fe685cf723422
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:07:47 +0800

    didn't need the float, finally done

--- Prog01.py
+++ Prog01.py
@@ -1,8 +1,8 @@
 #Create a program that ask user to input 2 numbers. Print the bigger.
 
 #inputting 2 numbers
-num_1 = float(input("Enter 1st Number: "))
-num_2 = float(input("Enter 2nd Number: "))
+num_1 = input("Enter 1st Number: ")
+num_2 = input("Enter 2nd Number: ")
 
 #Print the bigger number
 if num_1 > num_2:
---
+++ Prog02.py
@@ -0,0 +1,2 @@
+#Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+float(in)
