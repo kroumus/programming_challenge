51e3022fdd7d79f89999e22617d7dbf891291026
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:46:20 +0800

    print the result, finished

--- Prog06.py
+++ Prog06.py
@@ -4,3 +4,6 @@
 num_1 = int(input("Enter 1st Number: "))
 num_2 = int(input("Enter 2nd number: "))
 
+#Print the result when the first number is raised to the second number
+result = num_1 ** num_2
+print(result)
