79443b52595e481de195265f63ea94b083b08bb0
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:44:29 +0800

    input 2 numbers

--- Prog06.py
+++ Prog06.py
@@ -1,2 +1,6 @@
 #Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number
 
+#input 2 numbers
+num_1 = int(input("Enter 1st Number: "))
+num_2 = int(input("Enter 2nd number: "))
+
