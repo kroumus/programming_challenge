71995214618dec328060f632de81637cd366e09c
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:15:51 +0800

    finished? i forgot to commit the input code

--- Prog02.py
+++ Prog02.py
@@ -1,2 +1,13 @@
 #Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
-float(in)
+
+#input 2 numbers
+num_1 = input("Enter 1st Number: ")
+num_2 = input("Enter 2nd Number: ")
+
+#Print "Equal" when the numbers are the same
+if num_1 > num_2:
+    print((f"{num_1} is bigger!"))
+elif num_1 < num_2:
+    print((f"{num_2} is bigger!"))
+else:
+    print("Equal")
