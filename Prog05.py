ab8ef6fafaa9ef61dc836624e46e5306b9144748
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:40:07 +0800

    print quotient with decimal, finished

--- Prog05.py
+++ Prog05.py
@@ -4,3 +4,6 @@
 num_1 = float(input("Enter 1st Number: "))
 num_2 = float(input("Enter 2nd Number: "))
 
+#Print the quotient of the two numbers with the decimal point
+quotient = num_1 / num_2
+print(quotient)
