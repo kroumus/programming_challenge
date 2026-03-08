afa1196d53b86edcb090228c007875952d0fdea3
Rae <aldrichpadilla404@gmail.com>
2026-03-09 03:44:32 +0800

    not working

---
+++ Prog09.py
@@ -0,0 +1,8 @@
+#Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+even_numbers = []
+for i in range(101):
+    if i % 2 != 0:
+        even_numbers += 1
+        i.append(even_numbers)
+print(even_numbers)
