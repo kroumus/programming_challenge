eb725077aa434a64fd20bafca2e694d017ba710f
Rae <aldrichpadilla404@gmail.com>
2026-03-09 03:31:39 +0800

    printing the count of odd numbers

--- Prog08.py
+++ Prog08.py
@@ -1,5 +1,11 @@
 #Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 #input 10 numbers
+odd_count = 0
 for i in range(10):
     numbers = int(input(f"Enter number {i+1}: "))
+    if numbers % 2 != 0:
+        odd_count += 1 
+
+#Print how many are odd numbers
+print(odd_count)
