48d7396852d5887bc4d74f4b9bb2c9d2ca411bfb
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:35:18 +0800

    input function

--- Prog05.py
+++ Prog05.py
@@ -1,2 +1,6 @@
 #Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
+#input 2 numbers
+num_1 = float(input("Enter 1st Number: "))
+num_2 = float(input("Enter 2nd Number: "))
+
