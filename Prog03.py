6b388e3c3884021eb8332af193c36e4f6b19b859
Rae <aldrichpadilla404@gmail.com>
2026-03-09 02:27:41 +0800

    input function

--- Prog03.py
+++ Prog03.py
@@ -2,7 +2,7 @@
 
 #input 2 numbers
 num_1 = float(input("Enter 1st Number: "))
-num_2 = float(input("Enter 2st Number: "))
+num_2 = float(input("Enter 2nd Number: "))
 
 #Print the sum of the two numbers
 sum = num_1 + num_2
--- Prog04.py
+++ Prog04.py
@@ -1,2 +1,5 @@
 #Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
+#input 2 numbers
+num_1 = float(input("Enter 1st Number: "))
+num_2 = float(input("Enter 2nd Number: "))
