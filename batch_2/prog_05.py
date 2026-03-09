e6ee02d0f46a19ce670314a033809b59636a8390
Rae <aldrichpadilla404@gmail.com>
2026-03-09 22:09:21 +0800

    input 2 numbers

--- batch_2/prog_05.py
+++ batch_2/prog_05.py
@@ -1,2 +1,5 @@
 #Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number
 
+#input 2 numbers
+for i in range(2):
+    numbers = float(input(f"Enter Number {i+1}: "))
