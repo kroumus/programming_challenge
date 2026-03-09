873f46b94a79be17fae064fd68f286b3eef73776
Rae <aldrichpadilla404@gmail.com>
2026-03-09 18:41:48 +0800

    print not equal function done, finished

--- batch_2/prog_02.py
+++ batch_2/prog_02.py
@@ -1,6 +1,13 @@
 #Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same
 
 #input 2 numbers
+num_list = []
 for i in range(2):
     numbers = int(input(f"Enter Number {i+1}: "))
+    num_list.append(numbers)
 
+#print "Not Equal" when the numbers are not the same
+if num_list[0] != num_list[1]:
+    print("Not Equal")
+else:
+    print("Equal")
