8e0b67c603c284aa18c22380df11337a2dc3d6c2
Rae <aldrichpadilla404@gmail.com>
2026-03-09 18:31:54 +0800

    input 2 numbers

--- batch_2/prog_02.py
+++ batch_2/prog_02.py
@@ -1,2 +1,6 @@
 #Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same
 
+#input 2 numbers
+for i in range(2):
+    numbers = int(input(f"Enter Number {i+1}: "))
+
