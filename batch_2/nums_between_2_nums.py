12b2c9bbe85e45386abd834483b9f03c084d268d
Rae <aldrichpadilla404@gmail.com>
2026-03-09 23:50:45 +0800

    Print all the numbers between the two numbers, finished

--- batch_2/nums_between_2_nums.py
+++ batch_2/nums_between_2_nums.py
@@ -4,6 +4,13 @@
 PINK = '\033[38;2;255;182;193m'
 
 #input 2 numbers
+nums_in_between = []
 for i in range(2):
-    numbers = int(input(f"Enter Number {i+1}: "))
-    
+    numbers = int(input(f"{PINK}Enter Number {i+1}: "))
+    nums_in_between.append(numbers)
+if nums_in_between[0] < nums_in_between[1]:
+    for i in range(nums_in_between[0] + 1, nums_in_between[1]):
+        print(f"{PINK}", i, end=" ")
+else:
+    for i in range(nums_in_between[0] - 1, nums_in_between[1], -1):
+        print(f"{PINK}", i, end=" ")
