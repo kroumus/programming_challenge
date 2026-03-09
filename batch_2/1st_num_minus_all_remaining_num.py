ed7f86772f56d531648fdb7ba10a386ad2f5ccb6
Rae <aldrichpadilla404@gmail.com>
2026-03-09 22:47:10 +0800

    make it color pink daw sabi ng bebi ko

--- batch_2/1st_num_minus_all_remaining_num.py
+++ batch_2/1st_num_minus_all_remaining_num.py
@@ -1,9 +1,12 @@
 #Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
 #input 10 numbers
 initial = 0
 for i in range(10):
-    numbers = int(input(f"Enter Number {i+1}: "))
+    numbers = int(input(f"{PINK}Enter Number {i+1}: "))
     if initial == 0:
         initial = numbers
     else:
