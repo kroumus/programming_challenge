447d99ef7ade1cfab7328360ee2af1cfbd0f01c1
Rae <aldrichpadilla404@gmail.com>
2026-03-09 19:12:06 +0800

    Print the quotient of the two numbers without the decimal point, finished

--- batch_2/prog_04.py
+++ batch_2/prog_04.py
@@ -1,6 +1,11 @@
 #Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 #input 2 numbers 
+num_list = []
 for i in range(2):
     numbers = float(input(f"Enter Number {i+1}: "))
-    
+    num_list.append(numbers)
+
+#print the quotient of the two numbers without the decimal point
+quotient = num_list[0] / num_list[1]
+print(f"The quotient of the two numbers without the decimal point is {int(quotient)}")
