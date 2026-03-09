e02a2fdb7fc685f8df938be81728b5ffdb2e48c0
Rae <aldrichpadilla404@gmail.com>
2026-03-09 18:19:28 +0800

    print the smaller number, finished

--- batch_2/prog_1.py
+++ batch_2/prog_1.py
@@ -1,5 +1,13 @@
 #Create a program that ask user to input 2 numbers. Print the smaller number
 
 #input 2 numbers
+smaller_num = []
 for i in range(2):
-    numbers = int(input(f"Enter Number {i+1}: "))
+    number = int(input(f"Enter Number {i+1}: "))
+    smaller_num.append(number)
+
+#print the smallest number
+if smaller_num[0] < smaller_num[1]:
+    print(f"{smaller_num[0]} is smaller")
+else:
+    print(f"{smaller_num[1]} is smaller")
