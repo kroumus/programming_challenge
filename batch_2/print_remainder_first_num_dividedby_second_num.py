44422dc4b504952c983fadfc8955ed69825eb0b6
Rae <aldrichpadilla404@gmail.com>
2026-03-10 00:13:55 +0800

    print the remainder when the first number is divided by the second number, finished

--- batch_2/print_remainder_first_num_dividedby_second_num.py
+++ batch_2/print_remainder_first_num_dividedby_second_num.py
@@ -4,6 +4,11 @@
 PINK = '\033[38;2;255;182;193m'
 
 #input 2 number
+num_list = []
 for i in range(2):
-    numbers = float(input(f"{PINK}Enter Number {i+1}: "))
-    
+    numbers = int(input(f"{PINK}Enter Number {i+1}: "))
+    num_list.append(numbers)
+
+#print the remainder
+remainder = num_list[0] % num_list[1]
+print(remainder)
