b1c0cae94fc3a798dc8f8093e2d091f411b5251a
Rae <aldrichpadilla404@gmail.com>
2026-03-09 18:53:11 +0800

    print the difference of the two numbers, finished

--- batch_2/prog_03.py
+++ batch_2/prog_03.py
@@ -1,5 +1,11 @@
 #Create a program that ask user to input 2 numbers. Print the difference of the two numbers
 
 #input 2 numbers
+num_list = []
 for i in range(2):
     numbers = int(input(f"Enter Number {i+1}: "))
+    num_list.append(numbers)
+
+#print the difference of the two numbers
+difference = num_list[0] - num_list[1]
+print(f"The difference of the two numbers is {difference}")
