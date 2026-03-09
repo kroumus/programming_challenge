fc15ce4e29d86179b930e57cdb975c6c5f8e9e07
Rae <aldrichpadilla404@gmail.com>
2026-03-09 23:26:06 +0800

    input 2 numbers plus color pink

---
+++ batch_2/nums_between_2_nums.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#input 2 numbers
+for i in range(2):
+    numbers = int(input(f"Enter Number {i+1}: "))
+    
