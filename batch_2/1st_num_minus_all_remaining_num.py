4633d247107ae7d71983794b8a34ef8b51ea5aec
Rae <aldrichpadilla404@gmail.com>
2026-03-09 22:26:17 +0800

    input 10 numbers

--- batch_2/1st_num_minus_all_remaining_num.py
+++ batch_2/1st_num_minus_all_remaining_num.py
@@ -1,2 +1,5 @@
 #Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers
 
+#input 10 numbers
+for i in range(10):
+    numbers = int(input(f"Enter Number {i+1}: "))
