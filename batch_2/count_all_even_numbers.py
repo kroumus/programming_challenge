93ff73212c7e3e59bf7d9441a396abaae79127b9
Rae <aldrichpadilla404@gmail.com>
2026-03-09 22:56:39 +0800

    make it color pink daw sabi ng bebi ko

--- batch_2/count_all_even_numbers.py
+++ batch_2/count_all_even_numbers.py
@@ -1,5 +1,8 @@
 #Create a program that ask user to input 10 numbers. Print how many are even numbers
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
 #input 10 numbers
 for i in range(10):
     numbers = int(input(f"Enter Number {i+1}: "))
