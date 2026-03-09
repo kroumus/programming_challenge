68307a17a32d1b3a0ebd2397158349d03800fa09
Rae <aldrichpadilla404@gmail.com>
2026-03-10 00:09:56 +0800

    redoing this because it got deleted

---
+++ batch_2/print_remainder_first_num_dividedby_second_num.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#input 2 number
+for i in range(2):
+    numbers = float(input(f"{PINK}Enter Number {i+1}: "))
+    
