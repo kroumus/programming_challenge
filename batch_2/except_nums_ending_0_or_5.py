e4286304d991606d49bc79c4559cacae57032568
Rae <aldrichpadilla404@gmail.com>
2026-03-09 23:20:53 +0800

    print all the numbers starting from 0 to 100 except numbers ending in zero or ending five plus color pink, finished

--- batch_2/except_nums_ending_0_or_5.py
+++ batch_2/except_nums_ending_0_or_5.py
@@ -1,2 +1,10 @@
 #Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#prints all nums except nums ending 0 and 5
+for i in range(1, 101):
+    if i % 10 == 0 or i % 5 == 0:
+        continue
+    print(f"{PINK}",i, end=" ")
