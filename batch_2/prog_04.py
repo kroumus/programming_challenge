122a9f8e39dca00efdfe89e4977db0ff48a0c7db
Rae <aldrichpadilla404@gmail.com>
2026-03-09 19:06:00 +0800

    input 2 numbers

--- batch_2/prog_04.py
+++ batch_2/prog_04.py
@@ -1,1 +1,6 @@
 #Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+#input 2 numbers 
+for i in range(2):
+    numbers = float(input(f"Enter Number {i+1}: "))
+    
