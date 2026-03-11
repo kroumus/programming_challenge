2ab00fec6c28d4132682e31c1130b29dd0feec4f
Rae <aldrichpadilla404@gmail.com>
2026-03-12 01:12:48 +0800

    added color pink

--- batch_3/prog_02.py
+++ batch_3/prog_02.py
@@ -1,7 +1,10 @@
 #Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
 #input 10 numbers with list comprehension
-numbers = [int(input(f"Enter Number {i+1}: ")) for i in range(10)]
+numbers = [int(input(f"{PINK}Enter Number {i+1}: ")) for i in range(10)]
 
 #Display all numbers. For numbers with duplicate, display only the first entry using fromkeys() function I learned from gemini ai
 order_by_insertion = list(dict.fromkeys(numbers))
