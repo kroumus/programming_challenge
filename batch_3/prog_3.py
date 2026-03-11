76979229ce9d158fc874444359b0df2dbcf0bffd
Rae <aldrichpadilla404@gmail.com>
2026-03-12 02:00:41 +0800

    ahhh sleepy

--- batch_3/prog_3.py
+++ batch_3/prog_3.py
@@ -3,6 +3,9 @@
 #Display "Duplicate" after input when the inputted number have duplicate.
 
 #input a number, continue asking until the user input is invalid.
-while True:
-    numbers = input(f"Enter a Number: ")
+invalid = ["a", "b"]
 
+while True:
+    numbers = [input("Enter a Number: ")]
+  
+   
