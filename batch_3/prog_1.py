db80e0e7fe865240317c37aafbbf9888ee585c33
Rae <aldrichpadilla404@gmail.com>
2026-03-12 00:53:32 +0800

    I forgot to commit the steps but basically I used list comprehension that I learned 2 days ago and converted the list into a set so there will not be any dups and printed * and sep=" " with the variable containg the converted list into a set and I added pink color to it

--- batch_3/prog_1.py
+++ batch_3/prog_1.py
@@ -1,1 +1,11 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#input 10 numbers and list comrehension
+numbers = [int(input(f"{PINK}Enter Number {i+1}: ")) for i in range(10)]
+
+#diplay all numbers that don't have duplicate
+no_duplicate = set(numbers)
+print(*no_duplicate, sep=" ", end=" " )
