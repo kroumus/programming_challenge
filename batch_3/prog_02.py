c4460766a2175460846dff494546c935d5f8bce1
Rae <aldrichpadilla404@gmail.com>
2026-03-12 01:12:15 +0800

    Display all numbers. For numbers with duplicate, display only the first entry using fromkeys() function I learned from gemini ai

--- batch_3/prog_02.py
+++ batch_3/prog_02.py
@@ -2,3 +2,7 @@
 
 #input 10 numbers with list comprehension
 numbers = [int(input(f"Enter Number {i+1}: ")) for i in range(10)]
+
+#Display all numbers. For numbers with duplicate, display only the first entry using fromkeys() function I learned from gemini ai
+order_by_insertion = list(dict.fromkeys(numbers))
+print(*order_by_insertion)
