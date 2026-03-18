c95d428e094830d3986315edacb9c89d4a4c8d6b
Rae <aldrichpadilla404@gmail.com>
2026-03-18 23:01:06 +0800

    input 10 numbers using list comprehension

--- batch_4/prog_01.py
+++ batch_4/prog_01.py
@@ -1,2 +1,4 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
-[input()]
+
+#input 10 numbers
+numbers = [input(f"Enter Number {i+1}: ") for i in range(10)]
