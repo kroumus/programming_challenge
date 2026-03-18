83af0874bfd3a214085160a2ee5c423d8f27a12f
Rae <aldrichpadilla404@gmail.com>
2026-03-18 22:41:49 +0800

    just copied the code from the last prog

--- batch_3/prog_05.py
+++ batch_3/prog_05.py
@@ -1,3 +1,14 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid.
 #Display the number from lowest to highest. Clue: sort() function
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#input a number, continue asking until the user input is invalid.
+inputted = []
+while True:
+    numbers = input(f"{PINK}Enter a Number: ")
+    if not numbers.isdigit():
+        print("Invalid")
+        break
+
