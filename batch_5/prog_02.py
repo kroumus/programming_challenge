9c4d0c19ac66cde3006064cc30c1da97fdea9945
Rae <aldrichpadilla404@gmail.com>
2026-03-19 23:59:59 +0800

    chnaged it better

--- batch_5/prog_02.py
+++ batch_5/prog_02.py
@@ -9,13 +9,18 @@ PINK = '\033[38;2;255;182;193m'
 
 #ask the user to input a number (0-1000)
 numbers = int(input(f"{PINK}Enter Number (0-1000): "))
-
-
+#parameter (0-1000)
 if numbers < 0 or numbers > 1000:
     print("Invalid")
-elif numbers >= 0 or numbers <= 9:
-    print( print(f"00000{numbers}"))
+#for 1000 only 
 elif numbers == 1000:
     print(f"00{numbers}")
-else:
+#for 100 to 999
+elif numbers >= 100:
     print(f"000{numbers}")
+#for 10 to 99
+elif numbers >= 10:
+    print(f"0000{numbers}")
+#for 0 to 1
+else:
+    print(f"00000{numbers}")
