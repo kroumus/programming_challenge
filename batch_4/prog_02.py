6b7b6b3f9b69f7ffe9ec45d2f6ebda57c1605ad2
Rae <aldrichpadilla404@gmail.com>
2026-03-19 13:01:46 +0800

    input a number, continue asking until the user input is invalid

--- batch_4/prog_02.py
+++ batch_4/prog_02.py
@@ -1,3 +1,14 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. 
 #Display the number with the most number of duplicate.
 
+#input a number, continue asking until the user input is invalid.
+inputted = []
+while True:
+    numbers = input(f"Enter a Number: ")
+    if not numbers.isdigit():
+        print("Invalid")
+        break
+    
+    
+    inputted.append(numbers)
+    highest_duplicate = max(set(inputted), key=numbers.count)
