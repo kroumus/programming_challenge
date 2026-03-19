b376936d62c42553cd8eafe89229257d2dcd6dd0
Rae <aldrichpadilla404@gmail.com>
2026-03-19 13:40:01 +0800

    input a number, continue asking until the user input is invalid

--- batch_4/prog_03.py
+++ batch_4/prog_03.py
@@ -1,2 +1,13 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+
+#input a number, continue asking until the user input is invalid.
+
+while True:
+    numbers = input(f"{PINK}Enter a Number: ")
+    if not numbers.isdigit():
+        print("Invalid")
+        break
