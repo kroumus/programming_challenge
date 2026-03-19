0caba8dcff1ecaff5482b22acfc3462a188714c9
Rae <aldrichpadilla404@gmail.com>
2026-03-19 12:50:43 +0800

    added color pink

--- batch_4/display_all_nums_with_duplicate.py
+++ batch_4/display_all_nums_with_duplicate.py
@@ -1,9 +1,12 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
 #input 10 numbers
 duplicate = []
 unique = []
-numbers = [input(f"Enter Number {i+1}: ") for i in range(10)]
+numbers = [input(f"{PINK}Enter Number {i+1}: ") for i in range(10)]
 
 #Display all numbers that have duplicate
 for n in numbers:
