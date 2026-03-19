f736a8d2f8cc39edb640610d44b55410ab73fae6
Rae <aldrichpadilla404@gmail.com>
2026-03-19 12:22:10 +0800

    Display all numbers that have duplicate, finished

--- batch_4/prog_01.py
+++ batch_4/prog_01.py
@@ -1,4 +1,15 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 #input 10 numbers
+duplicate = []
+unique = []
 numbers = [input(f"Enter Number {i+1}: ") for i in range(10)]
+
+#Display all numbers that have duplicate
+for n in numbers:
+    if n in unique:
+        if n not in duplicate:
+            duplicate.append(n)
+    else:
+        unique.append(n)
+print(*duplicate)
