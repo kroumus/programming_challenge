67715a6de0bf8c684d970a05a69970d4ee5cbca8
Rae <aldrichpadilla404@gmail.com>
2026-03-19 23:50:13 +0800

    ask the user to input a number (0-1000)

--- batch_5/prog_02.py
+++ batch_5/prog_02.py
@@ -4,3 +4,18 @@
 #Input: 143
 #Output: 000143
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#ask the user to input a number (0-1000)
+numbers = int(input(f"{PINK}Enter Number (0-1000): "))
+
+
+if numbers < 0 or numbers > 1000:
+    print("Invalid")
+elif numbers >= 0 or numbers <= 9:
+    print( print(f"00000{numbers}"))
+elif numbers == 1000:
+    print(f"00{numbers}")
+else:
+    print(f"000{numbers}")
