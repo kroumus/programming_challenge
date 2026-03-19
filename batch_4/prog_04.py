c9b2f18f27d79f74b390c56b78c4ee6927f14f10
Rae <aldrichpadilla404@gmail.com>
2026-03-19 14:43:06 +0800

    just copied the code from batch 3 and added an argument reverse=Ture in sort() funtion

---
+++ batch_4/prog_04.py
@@ -0,0 +1,18 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid.
+#Display the number from highest to lowest. Clue: sort() function
+
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+#input a number, continue asking until the user input is invalid.
+inputted = []
+while True:
+    numbers = input(f"{PINK}Enter a Number: ")
+    if not numbers.isdigit():
+        print("Invalid")
+        break
+#Display the number from lowest to highest
+    inputted.append(int(numbers))
+    inputted.sort(reverse=True)
+    no_duplicate = list(dict.fromkeys(inputted))
+    print(*no_duplicate)
