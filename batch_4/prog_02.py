95c229d08bcfb784b54659fe3890161634c5bbd0
Rae <aldrichpadilla404@gmail.com>
2026-03-19 13:17:23 +0800

    Display the number with the most number of duplicate, added color pink, finshed

--- batch_4/prog_02.py
+++ batch_4/prog_02.py
@@ -1,14 +1,18 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. 
 #Display the number with the most number of duplicate.
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
 #input a number, continue asking until the user input is invalid.
 inputted = []
 while True:
-    numbers = input(f"Enter a Number: ")
+    numbers = input(f"{PINK}Enter a Number: ")
     if not numbers.isdigit():
         print("Invalid")
         break
     
-    
-    inputted.append(numbers)
-    highest_duplicate = max(set(inputted), key=numbers.count)
+#Display the number with the most number of duplicate    
+    inputted.append(int(numbers))
+    highest_duplicate = max(set(inputted), key=inputted.count)
+    print(highest_duplicate)
