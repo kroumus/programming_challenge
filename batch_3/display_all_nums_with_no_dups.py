86e8f5e15d2c499f7ecf485fc5ed5facf1ea4c17
Rae <aldrichpadilla404@gmail.com>
2026-03-20 12:55:25 +0800

    fixed this program using n for n in numbers if numbers.count(n) == 1

--- batch_3/display_all_nums_with_no_dups.py
+++ batch_3/display_all_nums_with_no_dups.py
@@ -7,5 +7,5 @@ PINK = '\033[38;2;255;182;193m'
 numbers = [int(input(f"{PINK}Enter Number {i+1}: ")) for i in range(10)]
 
 #diplay all numbers that don't have duplicate
-no_duplicate = set(numbers)
-print(*no_duplicate, sep=" ")
+no_duplicate = [n for n in numbers if numbers.count(n) == 1]
+print(*no_duplicate)
