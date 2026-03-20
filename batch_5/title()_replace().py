41f7ed6663cecc74a2a99e0556e7a1ec50b9d6d1
Rae <aldrichpadilla404@gmail.com>
2026-03-20 14:28:29 +0800

    finished prog using title() and replace()

--- batch_5/title()_replace().py
+++ batch_5/title()_replace().py
@@ -3,3 +3,8 @@
 #Input: jUAn DEla CrUZ
 #Output: JuanDelaCruz
 
+#make it color pink daw sabi ng bebi ko
+PINK = '\033[38;2;255;182;193m'
+
+fullname = input(f"{PINK}Enter your name: ").title().replace(" ", "")
+print(fullname)
